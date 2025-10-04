4b63e243cfceb4581c1b6e555eb352b453850922
정회민 <wony1102@naver.com>
2025-10-04 11:20:03 +0900

    init Project: generate project, set eslint, set prettier, set swagger api docs, set health-check

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Ditto API')
+    .setDescription('Quiz -> Result -> Email -> Share (MVP)')
+    .setVersion('0.1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
