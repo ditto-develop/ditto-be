e676652f72577ab312b8e7eec439a65ccd576cd6
정회민 <wony1102@naver.com>
2025-10-07 08:01:46 +0900

    feat: user entity, domain 생성

--- src/modules/user/users.module.ts
+++ src/modules/user/users.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../../infra/db/entities/user.entity';
 import { UsersController } from './presentation/users.controller';
+import { JwtTokenModule } from '../../shared/infrastructure/jwt/jwt.token.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity]), JwtTokenModule],
   controllers: [UsersController],
 })
 export class UsersModule {}
