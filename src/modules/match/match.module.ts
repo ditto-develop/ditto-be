c9da267c39249edaaf59da2867bbc3ce54e0da3c
정회민 <wony1102@naver.com>
2025-10-07 08:41:28 +0900

    feat: Match 컨트롤러 추가

--- src/modules/match/match.module.ts
+++ src/modules/match/match.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MatchController } from './presentation/match.controller';
+import { JwtTokenModule } from '../../shared/infrastructure/jwt/jwt.token.module';
 
-@Module({})
+@Module({
+  imports: [JwtTokenModule],
+  controllers: [MatchController],
+})
 export class MatchModule {}
