513a3678b8f581aed4f210ede613d6e4c32efd51
정회민 <wony1102@naver.com>
2025-10-09 16:02:37 +0900

    feat: api 에 query param 추가
    getSimilarUsersCount에 매치율을 matchRate query param으로 받도록 추가.

--- src/modules/match/presentation/match.controller.ts
+++ src/modules/match/presentation/match.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../../common/guards/jwt-auth.guard';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { SwaggerApiResponse } from '../../../common/decorators/swagger-api-response.decorator';
 import { CalculateResultRarityResponseDto } from './dto/calculate-result-rarity-response.dto';
 import { SimilarUsersCountResponseDto } from './dto/similar-users-count-response.dto';
@@ -14,10 +14,11 @@ export class MatchController {
   @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'x% 이상 일치한 사용자 수 조회 API' })
   @ApiBearerAuth('access-token')
+  @ApiQuery({ name: 'matchRate', required: false, type: Number, description: '매치율 (x%)' })
   @SwaggerApiResponse({ type: SimilarUsersCountResponseDto })
   @SwaggerApiResponse({ status: HttpStatus.UNAUTHORIZED })
   @SwaggerApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
-  getSimilarUsersCount(): SimilarUsersCountResponseDto {
+  getSimilarUsersCount(@Query('matchRate') matchRate?: string): SimilarUsersCountResponseDto {
     return {
       count: 4,
     };
