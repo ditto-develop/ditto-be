b6d9ca733ca6e5118bd1f603fe94199e341d323e
정회민 <wony1102@naver.com>
2025-10-09 15:43:26 +0900

    feat: ApiResponse에 대해 Success와 Fail의 경우 모두 문서에서 확인할 수 있도록 수정; feat: 에러 정의

--- src/common/decorators/swagger-api-response.decorator.ts
+++ src/common/decorators/swagger-api-response.decorator.ts
@@ -1,28 +1,61 @@
-import { ApiResponse, getSchemaPath } from '@nestjs/swagger';
-import { SuccessApiResponse } from '../dtos/api-response.dto';
-import { Type } from '@nestjs/common';
+import { ApiExtraModels, ApiResponse, getSchemaPath } from '@nestjs/swagger';
+import { FailApiResponse, SuccessApiResponse } from '../dtos/api-response.dto';
+import { applyDecorators, HttpStatus, Type } from '@nestjs/common';
+import { ApiResponseOptions } from '@nestjs/swagger/dist/decorators/api-response.decorator';
 
-export const SwaggerApiResponse = (config?: { status?: number; type?: Type; isArray?: boolean }) =>
-  ApiResponse({
-    status: config?.status ?? 200,
+export const SwaggerApiResponse = (config?: {
+  status?: HttpStatus;
+  type?: Type;
+  isArray?: boolean;
+  errorMessage?: string;
+}) => {
+  const fail = !!(config?.status && config.status >= HttpStatus.BAD_REQUEST);
+
+  const CustomApiResponse = !fail ? SuccessApiResponse : FailApiResponse;
+  const extraModels = [CustomApiResponse, ...(config?.type ? [config.type] : [])];
+
+  const errorExample: Partial<FailApiResponse> = {};
+  if (fail) {
+    switch (config?.status) {
+      case HttpStatus.UNAUTHORIZED:
+        errorExample.errorCode = 'UNAUTHORIZED';
+        errorExample.errMessage = '사용자 인증 실패';
+        break;
+      case HttpStatus.INTERNAL_SERVER_ERROR:
+        errorExample.errorCode = 'INTERNAL_SERVER_ERROR';
+        errorExample.errMessage = '서버 내부 오류';
+        break;
+      default:
+        errorExample.errorCode = 'UNKNOWN ERROR';
+        errorExample.errMessage = '알 수 없는 오류';
+    }
+    if (config.errorMessage) {
+      errorExample.errMessage = config.errorMessage;
+    }
+  }
+
+  const allOf: Array<Record<string, unknown>> = [{ $ref: getSchemaPath(CustomApiResponse) }];
+  if (!fail && config?.type) {
+    allOf.push({
+      properties: {
+        data: config?.isArray
+          ? { type: 'array', items: { $ref: getSchemaPath(config.type) } }
+          : { $ref: getSchemaPath(config.type) },
+      },
+    });
+  } else {
+    allOf.push({
+      properties: Object.fromEntries(Object.entries(errorExample).map(([key, value]) => [key, { example: value }])),
+    });
+  }
+
+  const options: ApiResponseOptions = {
+    status: config?.status ?? HttpStatus.OK,
     schema: {
       type: 'object',
-      allOf: [
-        { $ref: getSchemaPath(SuccessApiResponse) },
-        ...(config?.type
-          ? [
-              {
-                properties: {
-                  data: config.isArray
-                    ? {
-                        type: 'array',
-                        items: { $ref: getSchemaPath(config.type) },
-                      }
-                    : { $ref: getSchemaPath(config.type) },
-                },
-              },
-            ]
-          : []),
-      ],
+      allOf,
     },
-  });
+  };
+
+  return applyDecorators(ApiExtraModels(...extraModels), ApiResponse(options));
+};
--- src/modules/match/presentation/match.controller.ts
+++ src/modules/match/presentation/match.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../../common/guards/jwt-auth.guard';
-import { ApiBearerAuth, ApiExtraModels, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SwaggerApiResponse } from '../../../common/decorators/swagger-api-response.decorator';
 import { CalculateResultRarityResponseDto } from './dto/calculate-result-rarity-response.dto';
 import { SimilarUsersCountResponseDto } from './dto/similar-users-count-response.dto';
 
 @ApiTags('Match')
 @Controller('match')
-@ApiExtraModels(SimilarUsersCountResponseDto, CalculateResultRarityResponseDto)
 export class MatchController {
   constructor() {}
 
@@ -16,6 +15,8 @@ export class MatchController {
   @ApiOperation({ summary: 'x% 이상 일치한 사용자 수 조회 API' })
   @ApiBearerAuth('access-token')
   @SwaggerApiResponse({ type: SimilarUsersCountResponseDto })
+  @SwaggerApiResponse({ status: HttpStatus.UNAUTHORIZED })
+  @SwaggerApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
   getSimilarUsersCount(): SimilarUsersCountResponseDto {
     return {
       count: 4,
@@ -27,6 +28,8 @@ export class MatchController {
   @ApiOperation({ summary: '게임 결과 희귀도 계산 API' })
   @ApiBearerAuth('access-token')
   @SwaggerApiResponse({ type: CalculateResultRarityResponseDto })
+  @SwaggerApiResponse({ status: HttpStatus.UNAUTHORIZED })
+  @SwaggerApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
   calculateResultRarity(): CalculateResultRarityResponseDto {
     return {
       rarity: 15,
